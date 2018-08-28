5a5f2304795a8474e71759b9217426f147c7fa95
Juniorcoliseu <juniorcoliseu@msn.com>
2018-08-27 22:12:03 -0300

    Class produto quebrando

---
+++ src/main/java/com/skyyl/ecommerce/service/dto/ProdutoDTO.java
@@ -0,0 +1,77 @@
+package com.skyyl.ecommerce.service.dto;
+
+public class ProdutoDTO {
+	
+	
+	private Long id;
+	
+	private String nome;
+	
+	private String cor;
+	
+	private String tamanho;
+	
+	private String descricao;
+	
+	private String tipo;
+	
+	private Float valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Float getValor() {
+		return valor;
+	}
+
+	public void setValor(Float valor) {
+		this.valor = valor;
+	} 
+
+
+}
